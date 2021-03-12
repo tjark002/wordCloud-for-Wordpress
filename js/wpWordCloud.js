69682d762c92038657540c236f581f46cc3d1599
Nils Haffke <nilsrazr@gmail.com>
2021-03-12 16:29:47 +0100

    window resize / mobile grid size

--- js/wpWordCloud.js
+++ js/wpWordCloud.js
@@ -1,5 +1,20 @@
 (function ($) {
 
+	// Responsive Function (trigger before drawing canvas) 
+	function canvasResize() {   
+		$canvas = $(".word-cloud"); 
+		var parentWidth = $canvas.parent().outerWidth();
+		var parentHeight =  $canvas.parent().outerHeight();
+		$canvas.attr('width', parentWidth);
+		$canvas.attr('height', parentWidth * 1.5);
+		console.log("canvas resize");
+	}
+
+	// Trigger canvas resize function on window resize
+	$(window).on('resize', function() {                      
+		canvasResize();
+	});
+
 	'use strict';
 
 	// init
@@ -269,6 +284,9 @@
 
 	$('.render-word-cloud').click(function() {
 
+		// canvas container resize
+		canvasResize();
+
 		$("canvas.word-cloud").css("height", "auto");
 		$(".word-cloud-example").hide();
 		$(".canvas-download-button").css("display", "inline-block");
@@ -519,6 +537,3 @@
 	}
 
 })(jQuery);
-
-
-
