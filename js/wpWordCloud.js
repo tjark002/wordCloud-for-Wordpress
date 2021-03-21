081c64075316933610de00fdef3373fbf5e9fab7
Tim Krieger <krieger@nlq.nibis.de>
2021-03-21 16:11:08 +0100

    Adjust slider values

--- js/wpWordCloud.js
+++ js/wpWordCloud.js
@@ -146,9 +146,9 @@
 
 	// slider change function
 	$('#cloud-slider').change(function() {
-		// 160, 140, 120, 100, 80, 60
+		// 150, 125, 100, 75, 50, 25
 		var theVal = $(this).val() - 1;
-		var values = [160, 140, 120, 100, 80, 60];
+		var values = [150, 125, 100, 75, 50, 25];
 		$(".word-cloud-setting-size-factor").val(values[theVal]); 
 	});
 
